825687ca6989e6729793392bd01a3d078d9d5bb8
Jyotir Aditya Giri <58664120+joe-aditya@users.noreply.github.com>
2021-06-15 10:37:23 +0530

    test1

--- App.js
+++ App.js
@@ -10,11 +10,12 @@ const _console = _.clone(console);
 console.warn = (message) => {
   if (message.indexOf("Setting a timer") <= -1) {
     _console.warn(message);
+    _console.warn(message);
   }
 };
 
 export default function App() {
-  console.log("RENDERING App");
+  console.log("RENDERING");
 
   return (
     <Provider store={store}>
